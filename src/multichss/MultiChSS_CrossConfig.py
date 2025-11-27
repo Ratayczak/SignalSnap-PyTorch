65fce08edb8769593c51ce41caa7fc5e79a90aaf
Armin Etemad <armin.ghorbanietemad@rub.de>
2025-11-27 16:11:16 +0100

    Adding type-aware notation

--- src/multichss/MultiChSS_CrossConfig.py
+++ src/multichss/MultiChSS_CrossConfig.py
@@ -5,11 +5,25 @@
 # For details, see the LICENSE file in the root of this repository or
 # https://opensource.org/licenses/BSD-3-Clause
 
+from typing import Optional, List, Tuple
+
 class InvalidConfigError(Exception):
     pass
 
 class CrossConfig:
-    def __init__(self, auto_corr=True, cross_corr_2=None, cross_corr_3=None, cross_corr_4=None):
+    auto_corr: bool
+    cross_corr_2: Optional[List[Tuple[int, int]]]
+    cross_corr_3: Optional[List[Tuple[int, int, int]]]
+    cross_corr_4: Optional[List[Tuple[int, int, int, int]]]
+
+    def __init__(
+        self,
+        auto_corr: bool = True,
+        cross_corr_2: Optional[List[Tuple[int, int]]] = None,
+        cross_corr_3: Optional[List[Tuple[int, int, int]]] = None,
+        cross_corr_4: Optional[List[Tuple[int, int, int, int]]] = None,
+    ) -> None:
+
         self.auto_corr = auto_corr
         self.cross_corr_2 = cross_corr_2
         self.cross_corr_3 = cross_corr_3
@@ -17,7 +31,53 @@ class CrossConfig:
 
         self.validate()
 
-    def validate(self):
-        if not isinstance(self.auto_corr, (bool)):
-            raise InvalidConfigError(f"Invalid 'auto_corr': {self.auto_corr}.\n"
-                                     f"Must be boolian.")
+    def validate(self) -> None:
+        if not isinstance(self.auto_corr, bool):
+            raise InvalidConfigError(
+                f"Invalid 'auto_corr': {self.auto_corr}. Must be boolean."
+            )
+
+        # validate cross_corr_2
+        if self.cross_corr_2 is not None:
+            if not (
+                isinstance(self.cross_corr_2, list)
+                and all(
+                    isinstance(t, tuple)
+                    and len(t) == 2
+                    and all(isinstance(i, int) for i in t)
+                    for t in self.cross_corr_2
+                )
+            ):
+                raise InvalidConfigError(
+                    "'cross_corr_2' must be list of 2-tuples of ints"
+                )
+
+        # validate cross_corr_3
+        if self.cross_corr_3 is not None:
+            if not (
+                isinstance(self.cross_corr_3, list)
+                and all(
+                    isinstance(t, tuple)
+                    and len(t) == 3
+                    and all(isinstance(i, int) for i in t)
+                    for t in self.cross_corr_3
+                )
+            ):
+                raise InvalidConfigError(
+                    "'cross_corr_3' must be list of 3-tuples of ints"
+                )
+
+        # validate cross_corr_4
+        if self.cross_corr_4 is not None:
+            if not (
+                isinstance(self.cross_corr_4, list)
+                and all(
+                    isinstance(t, tuple)
+                    and len(t) == 4
+                    and all(isinstance(i, int) for i in t)
+                    for t in self.cross_corr_4
+                )
+            ):
+                raise InvalidConfigError(
+                    "'cross_corr_4' must be list of 4-tuples of ints"
+                )
