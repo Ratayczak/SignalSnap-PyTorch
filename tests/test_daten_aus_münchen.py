5070d3166d8e0fc8680dc0e4cf0aa45fbedb5139
David Ratayczak <ratayczakdavid@gmail.com>
2026-06-26 13:27:24 +0200

    Implemented second test between old and new api. Using the old window cross spectra coincide

--- tests/test_daten_aus_münchen.py
+++ tests/test_daten_aus_münchen.py
@@ -8,7 +8,7 @@ from multichss.pipelines import calculate_spectra
 import h5py
 
 
-def test_new_vs_old_api_1():
+def test_new_vs_old_api_auto_corr_1():
     """
     Tests if the refactor implements the same calculations. To check if the
     implementation is correct, the old window function needs to be used in
@@ -35,7 +35,7 @@ def test_new_vs_old_api_1():
         s3_calc="1/4",
         backend="cuda",
         order_in=[1, 2, 3, 4],
-        spectrum_size=600,
+        spectrum_size=1000,
         show_first_frame=False,
         old_window=True,
     )
@@ -51,7 +51,7 @@ def test_new_vs_old_api_1():
     result4 = result_store.get((0, 0, 0, 0), 4)
 
     benchmark_spectra = np.load(
-        "./tests/test_data/erste_daten_aus_münchen.npz", allow_pickle=True
+        "./tests/test_data/erste_daten_aus_münchen_auto_corr.npz", allow_pickle=True
     )
     old_spectra = benchmark_spectra["spectra"].item()
     old_error = benchmark_spectra["error"].item()
@@ -224,3 +224,157 @@ def test_new_vs_old_api_1():
     np.testing.assert_allclose(
         np.asarray(result4_ch1.freq), np.asarray(old_freqs[1][4]), rtol=0, atol=1e-12
     )
+
+
+def test_new_vs_old_api_cross_corr_1():
+    """
+    Tests if the refactor implements the same calculations. To check if the
+    implementation is correct, the old window function needs to be used in
+    SpectrumConfig.
+    """
+    with h5py.File(
+        "C:/Users/david/Masterarbeit/data/5Qubit/raw/DRaw_C_Te_v0.h5", "r"
+    ) as f:
+        x_test_dataset = f["/X_test"]
+        assert isinstance(x_test_dataset, h5py.Dataset)
+        X_test = x_test_dataset[...]
+
+    signal_channel_0 = X_test[:, :, 0].reshape(-1)
+    signal_channel_1 = X_test[:, :, 1].reshape(-1)
+    signal_channel_0 = signal_channel_0[:1000000]
+    signal_channel_1 = signal_channel_1[:1000000]
+    dconfig1 = DataConfig(data=signal_channel_0, dt=2.0, t_unit="ns")
+    dconfig2 = DataConfig(data=signal_channel_1, dt=2.0, t_unit="ns")
+    selected_data = [0, 1]
+
+    sconfig = SpectrumConfig(
+        f_min=-0.25,
+        f_max=0.25,
+        s3_calc="1/4",
+        backend="cuda",
+        order_in=[1, 2, 4],
+        spectrum_size=1000,
+        show_first_frame=False,
+        old_window=True,
+    )
+
+    cconfig = CrossConfig(
+        auto_corr=False,
+        cross_corr_2=[(0, 1), (1, 0)],
+        cross_corr_4=[(1, 0, 0, 1), (1, 1, 0, 0)],
+    )
+
+    result_store = calculate_spectra(
+        sconfig, cconfig, [dconfig1, dconfig2], selected=selected_data
+    )
+
+    result_01_2 = result_store.get((0, 1), 2)
+    result_10_2 = result_store.get((1, 0), 2)
+    result_1001_4 = result_store.get((1, 0, 0, 1), 4)
+    result_1100_4 = result_store.get((1, 1, 0, 0), 4)
+
+    sconfig2 = SpectrumConfig(
+        f_min=0,
+        f_max=0.25,
+        s3_calc="1/2",
+        backend="cuda",
+        order_in=[3],
+        spectrum_size=1000,
+        show_first_frame=False,
+        old_window=True,
+    )
+
+    cconfig2 = CrossConfig(
+        auto_corr=False, cross_corr_3=[(0, 1, 1), (1, 0, 0), (0, 0, 1)]
+    )
+
+    result_store2 = calculate_spectra(
+        sconfig2, cconfig2, [dconfig1, dconfig2], selected=selected_data
+    )
+
+    result_011_3 = result_store2.get((0, 1, 1), 3)
+    result_100_3 = result_store2.get((1, 0, 0), 3)
+    result_001_3 = result_store2.get((0, 0, 1), 3)
+
+    benchmark_spectra_ch124 = np.load(
+        "./tests/test_data/erste_daten_aus_münchen_cross_corr_ch124.npz", allow_pickle=True
+    )
+    old_spectra_ch124 = benchmark_spectra_ch124["spectra"].item()
+    old_error_ch124 = benchmark_spectra_ch124["error"].item()
+    old_freqs_ch124 = benchmark_spectra_ch124["freqs"].item()
+
+    benchmark_spectra_ch3 = np.load(
+        "./tests/test_data/erste_daten_aus_münchen_cross_corr_ch3.npz", allow_pickle=True
+    )
+    old_spectra_ch3 = benchmark_spectra_ch3["spectra"].item()
+    old_error_ch3 = benchmark_spectra_ch3["error"].item()
+    old_freqs_ch3 = benchmark_spectra_ch3["freqs"].item()
+
+    cross_corr_results_ch124 = [
+        ((0, 1), 2, result_01_2),
+        ((1, 0), 2, result_10_2),
+        ((1, 0, 0, 1), 4, result_1001_4),
+        ((1, 1, 0, 0), 4, result_1100_4),
+    ]
+
+    for channels, order, result in cross_corr_results_ch124:
+        assert result.spectrum is not None
+        np.testing.assert_allclose(
+            np.asarray(result.spectrum),
+            np.asarray(old_spectra_ch124[channels][order]),
+            rtol=1e-6,
+            atol=1e-8,
+            err_msg=f"{order}-order spectrum {channels} doesn't match",
+        )
+
+        assert result.spectrum_error is not None
+        np.testing.assert_allclose(
+            np.asarray(result.spectrum_error),
+            np.asarray(old_error_ch124[channels][order]),
+            rtol=1e-6,
+            atol=1e-8,
+            err_msg=f"{order}-order spectrum error {channels} doesn't match",
+        )
+
+        assert result.freq is not None
+        np.testing.assert_allclose(
+            np.asarray(result.freq),
+            np.asarray(old_freqs_ch124[channels][order]),
+            rtol=0,
+            atol=1e-12,
+            err_msg=f"{order}-order frequencies {channels} don't match",
+        )
+
+    cross_corr_results_ch3 = [
+        ((0, 1, 1), 3, result_011_3),
+        ((1, 0, 0), 3, result_100_3),
+        ((0, 0, 1), 3, result_001_3),
+    ]
+
+    for channels, order, result in cross_corr_results_ch3:
+        assert result.spectrum is not None
+        np.testing.assert_allclose(
+            np.asarray(result.spectrum),
+            np.asarray(old_spectra_ch3[channels][order]),
+            rtol=1e-6,
+            atol=1e-8,
+            err_msg=f"{order}-order spectrum {channels} doesn't match",
+        )
+
+        assert result.spectrum_error is not None
+        np.testing.assert_allclose(
+            np.asarray(result.spectrum_error),
+            np.asarray(old_error_ch3[channels][order]),
+            rtol=1e-6,
+            atol=1e-8,
+            err_msg=f"{order}-order spectrum error {channels} doesn't match",
+        )
+
+        assert result.freq is not None
+        np.testing.assert_allclose(
+            np.asarray(result.freq),
+            np.asarray(old_freqs_ch3[channels][order]),
+            rtol=0,
+            atol=1e-12,
+            err_msg=f"{order}-order frequencies {channels} don't match",
+        )
